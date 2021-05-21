5a71ebff92c1a93c315b57f9e3b3fa2e5e53a433
kangmingu <kangmin1012@naver.com>
2021-05-21 17:19:29 +0900

    release 1.0.1

--- app/src/main/java/min/dev/singleclick/MainActivity.kt
+++ app/src/main/java/min/dev/singleclick/MainActivity.kt
@@ -12,13 +12,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        binding.txtTest.setOnSingleClickListener(600) {
-            //do Something
-        }
-
-        binding.txtTest2.setOnSingleClickListener(2000) {
-            //do Something
-        }
     }
 }
--- singleclick/src/main/java/min/dev/singleclick/Extension.kt
+++ singleclick/src/main/java/min/dev/singleclick/Extension.kt
@@ -2,7 +2,7 @@ package min.dev.singleclick
 
 import android.view.View
 
-fun View.setOnSingleClickListener(interval: Long = 500, onSingleClick: (v: View?) -> Unit) {
+fun View.mingSingleClickListener(interval: Long = 500, onSingleClick: (v: View?) -> Unit) {
     this.setOnClickListener(object : OnSingleClickListener(interval) {
         override fun onSingleClick(v: View?) {
             onSingleClick(v)
